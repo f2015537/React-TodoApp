60cd7e845fc2e4d44ba3d17943658ed7c4ed80f6
f2015537 <divyampatro1997@gmail.com>
2018-10-15 02:58:11 +0530

    Done lesson 24

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ class App extends Component {
       {id: 2, content: 'play mario kart'}
     ]
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
       <h1 className="center blue-text">Todos</h1>
-      <Todos todos={this.state.todos}/> 
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/> 
       </div>
     );
   }
